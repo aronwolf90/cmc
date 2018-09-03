93d3ae5a8aea9f1a8b665807b8da900df1bc6f0b
Aron Wolf <aronwolf90@gmail.com>
2018-09-03 16:56:03 +0000

    fix(sql): has_many records on ProjectRecordDay RecordDay
    why:
    time zone cause wrong values on the has_many of records
    on ProjectRecordDay RecordDay
    
    This change addresses the need by:
    add beginning_of_day in the has many relation

--- app/models/project_record_day.rb
+++ app/models/project_record_day.rb
@@ -3,7 +3,7 @@
 class ProjectRecordDay < ApplicationRecord
   belongs_to :project
   has_many :records, (lambda do |record_day|
-    where(start_time: record_day.day...record_day.day.end_of_day).ordered
+    where(start_time: record_day.day.beginning_of_day...record_day.day.end_of_day).ordered
   end), through: :project
 
   scope :ordered, -> { order(day: :desc) }
--- app/models/record_day.rb
+++ app/models/record_day.rb
@@ -3,7 +3,7 @@
 class RecordDay < ApplicationRecord
   belongs_to :user
   has_many :records, (lambda do |record_day|
-    where(start_time: record_day.day...record_day.day.end_of_day).ordered
+    where(start_time: record_day.day.beginning_of_day...record_day.day.end_of_day).ordered
   end), through: :user
 
   scope :ordered, -> { order(day: :desc) }
