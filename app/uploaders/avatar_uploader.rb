bb2f0fe65763216000cf9fa3a96d51e867f8025a
aron <aronwolf90@gmail.com>
2019-07-06 14:00:20 +0200

    feature: move to object storage
    We are preparing the app to by ready
    for a large number of users. The object
    storage is scalable.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,14 +5,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{Organization.current}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if ["AWS_ENABLED"]
+      "#{Organization.current}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      "uploads/#{Organization.current}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn"t been a file uploaded:
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+CarrierWave.configure do |config|
+  if ENV["AWS_ENABLED"].present?
+    config.fog_provider = "fog/aws"
+    config.fog_credentials = {
+      provider:              "AWS",
+      aws_access_key_id:     ENV["AWS_KEY"],
+      aws_secret_access_key: ENV["AWS_SECRET"],
+      region:                ENV["AWS_REGION"],
+      endpoint:              "https://#{ENV["AWS_ENDPOINT"]}",
+      path_style:            true
+    }
+    config.fog_directory = ENV["AWS_DIRECTORY"]
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
+end
