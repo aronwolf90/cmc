5c5e872398242ba18f20fbdd347895a0280abb83
Aron Wolf <aronwolf90@gmail.com>
2020-07-17 01:00:18 +0200

    feature: allow import google events
    **Why**: To allow edit the calender from google.

--- spec/operations/google_calenders/export_event_operation_spec.rb
+++ spec/operations/google_calenders/export_event_operation_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe GoogleCalenders::ExportEventOperation do
   subject(:call) do
-    described_class.call(event: event)
+    described_class.call(event: event, organization: organization)
   end
 
   let(:event) do
@@ -13,18 +13,29 @@ RSpec.describe GoogleCalenders::ExportEventOperation do
       description: "Description",
       google_calender_event_id: google_calender_event_id,
       start_time: "10.10.2020 10:10:00",
-      end_time: "10.10.2020 10:20:00"
+      end_time: "10.10.2020 10:20:00",
+      updated_at: Time.zone.now
     )
   end
   let(:organization) { Organization.new(google_calender_id: "id") }
+  let(:google_authorization_data) do
+    GoogleAuthorizationData.new(
+      access_token: "token",
+      expires_at: 1.hour.from_now.to_datetime,
+      refresh_token: "refresh_token"
+    )
+  end
 
   before do
     allow(GoogleCalenderClient).to receive(:create_event).and_return(double(id: "id"))
     allow(GoogleCalenderClient).to receive(:update_event).and_return(double(id: "id"))
-    allow(Organization).to receive(:current).and_return(organization)
+    allow(GoogleCalenderClient).to receive(:get_event).and_return(double(id: "id", updated: 1.hour.ago))
+    allow(GoogleCalenderClient).to receive(:authorize!).and_return(google_authorization_data)
+    allow(GoogleCalenders::AuthorizeOperation).to receive(:call)
+      .and_return(google_authorization_data: google_authorization_data)
   end
 
-  context "when Event#google_calender_event_id is present" do
+  context "when Event#google_calender_event_id is nil" do
     let(:google_calender_event_id) { nil }
 
     specify do
@@ -45,7 +56,7 @@ RSpec.describe GoogleCalenders::ExportEventOperation do
     end
   end
 
-  context "when Event#google_calender_event_id is nil" do
+  context "when Event#google_calender_event_id is present" do
     let(:google_calender_event_id) { "event_id" }
 
     specify do
@@ -64,6 +75,19 @@ RSpec.describe GoogleCalenders::ExportEventOperation do
           },
           google_authorization_data: organization.google_calender_authorization_data
       )
+    end
+  end
+
+  context "when Event#google_calender_event_id is present and updated is in the future" do
+    let(:google_calender_event_id) { "event_id" }
+
+    before do
+      allow(GoogleCalenderClient).to receive(:get_event).and_return(double(id: "id", updated: 1.hour.from_now))
+    end
+
+    specify do
+      call
+      expect(GoogleCalenderClient).not_to have_received(:update_event)
     end
   end
 end
---
+++ spec/operations/google_calenders/import_event_operation_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe GoogleCalenders::ImportEventOperation do
+  subject(:call) do
+    described_class.(
+      google_calender_event_id: google_calender_event_id,
+      organization: organization
+    )
+  end
+
+  let(:organization) { Organization.new(google_calender_id: "id") }
+  let(:google_authorization_data) do
+    GoogleAuthorizationData.new(
+      access_token: "token",
+      expires_at: 1.hour.from_now.to_datetime,
+      refresh_token: "refresh_token"
+    )
+  end
+  let(:event) { Event.new }
+
+  before do
+    allow(GoogleCalenderClient)
+      .to receive(:get_event)
+      .and_return(double(
+        id: "id",
+        updated: 1.hour.ago,
+        summary: "Title",
+        start: 1.hour.ago,
+        end: 1.hour.from_now,
+        description: "description"
+      )
+    )
+    allow(GoogleCalenders::AuthorizeOperation).to receive(:call)
+      .and_return(google_authorization_data: google_authorization_data)
+    allow(Event).to receive(:find_or_initialize_by).and_return(event)
+  end
+
+  context "when Event#google_calender_event_id is nil" do
+    let(:google_calender_event_id) { nil }
+
+    specify do
+      expect(event).to receive(:update!)
+      call
+    end
+  end
+end
