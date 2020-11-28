7e8411670ccffda5a237bbd1d5620ef196acea4b
Aron Wolf <aronwolf90@gmail.com>
2020-11-28 23:46:45 +0100

    test: add monkey tests to production test
    Why: to see if something crashes

--- bin/production_tests.rb
+++ bin/production_tests.rb
@@ -91,5 +91,28 @@ click_on "Add payment informations"
 sleep 1
 !(page.has_text?("Kartendaten speichern") || page.has_text?("Card information")) &&
   raise("Stripe does not work")
+%w[administration administration/board_lists administration/projects administration/projects/1 administration/wiki administration/users/1 administration/contacts/1 administration/archive].each do |page|
+  visit("https://test-organization.ticktensio.com/#{page}")
+  page.evaluate_script(%[
+      (function() {
+          function callback() {
+              gremlins.createHorde({
+                  species: [gremlins.species.clicker()],
+                  mogwais: [gremlins.mogwais.alert(),gremlins.mogwais.fps(),gremlins.mogwais.gizmo()],
+                  strategies: [gremlins.strategies.distribution()]
+              }).unleash();
+          }
+          var s = document.createElement("script");
+          s.src = "https://unpkg.com/gremlins.js";
+          if (s.addEventListener) {
+              s.addEventListener("load", callback, false);
+          } else if (s.readyState) {
+              s.onreadystatechange = callback;
+          }
+          document.body.appendChild(s);
+      })()
+  ])
+  sleep 10
+end
 
 puts "success"
