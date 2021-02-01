693a74f60caea2384ca0a7db8f5da3b4c759aac3
Aron Wolf <aronwolf90@gmail.com>
2021-02-01 23:19:25 +0100

    feature: make csv more excel compatible
    Excel hat problem with ',' as spearator. The
    solution is to use ';' as seperator.

--- app/exporters/base_csv_exporter.rb
+++ app/exporters/base_csv_exporter.rb
@@ -3,6 +3,15 @@
 class BaseCsvExporter
   include LpCSVExportable::CanExportAsCSV
 
+  def to_csv
+    CSV.generate(nil, col_sep: ";", force_quotes: true) do |csv|
+      csv << headers
+      data_matrix.each do |row|
+        csv << row
+      end
+    end
+  end
+
   def self.call(collection)
     new.tap do |exporter|
       exporter.collection = collection
